ee81b18af633ea6017a642f9c4e51bfe719042bc
Rohit Chormale <rohitchormale@gmail.com>
2018-10-31 14:15:10 +0530

    Added README.md

--- {{cookiecutter.project_slug}}/wsgi.py
+++ {{cookiecutter.project_slug}}/wsgi.py
@@ -1,8 +1,9 @@
 """
 wsgi application instance to deploy app behind apache/gunicorn/nginx
 
-@author: Rohit Chormale
+@author: {{ cookiecutter.author }}
 """
+
 import os, sys
 app_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, app_path)
