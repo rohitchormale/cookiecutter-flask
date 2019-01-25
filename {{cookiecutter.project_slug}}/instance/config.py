dfe0b61fbcc809fc9330ef882376e955adc2de1f
Rohit Chormale <rohitchormale@gmail.com>
2019-01-25 18:29:56 +0530

    Updated config

--- {{cookiecutter.project_slug}}/instance/config.py
+++ {{cookiecutter.project_slug}}/instance/config.py
@@ -2,6 +2,11 @@
 Add variable config here. Do NOT commit this file in vcs.
 """
 
+
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+
 #####################
 # Development Server
 ####################
@@ -9,6 +14,25 @@ Add variable config here. Do NOT commit this file in vcs.
 DEBUG = True
 HOST = "0.0.0.0"
 PORT = 8080
+
+
+###########
+# Database
+###########
+
+SQLALCHEMY_DATABASE_URI = "sqlite:///{{cookiecutter.project_slug}}.sqlite3"
+# SQLALCHEMY_DATABASE_URI = "mysql:/user:passwd@127.0.0.1:3307/"
+
+
+###########
+# Logging
+##########
+
+import logging
+LOG_FILE = os.path.join(BASE_DIR, "log", "{{cookiecutter.project_slug}}.log")
+LOG_SIZE = 1024*1024
+LOG_LEVEL = logging.DEBUG
+
 
 ########
 # Auth
@@ -18,11 +42,6 @@ PORT = 8080
 SECRET_KEY = "my-secret-key"
 
 
-###########
-# Database
-###########
-SQLALCHEMY_DATABASE_URI = "sqlite:///{{cookiecutter.project_slug}}.sqlite3"
-# SQLALCHEMY_DATABASE_URI = "mysql:/user:passwd@127.0.0.1:3307/"
 
 
 
