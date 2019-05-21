924f4290231a50e8185bba879918aaf757b1f2bc
Rohit Chormale <rohitchormale@gmail.com>
2019-05-21 13:44:41 +0530

    Removed development.py as better way is 'flask run'

--- {{cookiecutter.project_slug}}/development.py
+++
@@ -1,8 +0,0 @@
-"""
-Development server
-
-@author: {{ cookiecutter.author }}
-"""
-from {{cookiecutter.package_slug}} import create_app
-app = create_app()
-app.run(host=app.config["HOST"], port=app.config["PORT"], debug=app.config["DEBUG"])
